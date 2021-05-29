e67282b55149ac83400e49c15cf6877563adc64e
Suren Rodrigo <bksdrodrigo@outlook.com>
2021-05-29 10:23:03 +0530

    Base express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export const Greeter = (name: string) => `Hello ${name} Welcome to Node!!`;
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+if (process.env.NODE_ENV !== 'production') dotenv.config();
+const port  = process.env.PORT || 3000;
+const hostname = process.env.HOST || '0.0.0.0';
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, hostname, () => {
+  console.log(`Application running on http://${hostname}:${port}`)
+})
